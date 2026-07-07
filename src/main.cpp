8cbb9e2812581f942436254785908cd26d5eff08
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-07 14:13:38 +0600

    Added necessary structs

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,80 @@
-#include <bits/stdc++.h>
 #include <raylib.h>
 
-using namespace std;
+struct Player
+{
+    Vector2 position;
+    Vector2 velocity;
+
+    Rectangle body;
+    Rectangle attack;
+
+    bool grounded;
+    bool facingRight;
+
+    int health;
+};
+
+struct Enemy
+{
+    Vector2 position;
+    Vector2 velocity;
+
+    Rectangle body;
+
+    int health;
+};
+
+struct Game
+{
+    Player player;
+
+    Enemy enemies[20];
+
+    Camera2D camera;
+};
 
 int main()
 {
-    InitWindow(800, 500, "Colorist");
-
+    InitWindow(1280, 720, "Colorist");
     SetTargetFPS(60);
 
-    while(!WindowShouldClose())
+    Game game;
+
+    game.player.position = {100, 300};
+    game.player.velocity = {0, 0};
+
+    game.player.body = {game.player.position.x, game.player.position.y, 32, 64};
+
+    game.player.health = 100;
+    game.player.grounded = false;
+    game.player.facingRight = true;
+
+    while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_D))
+        {
+            game.player.position.x += 5;
+            game.player.facingRight = true;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            game.player.position.x -= 5;
+            game.player.facingRight = false;
+        }
+
+        if (IsKeyDown(KEY_W)) game.player.position.y -= 5;
+
+        if (IsKeyDown(KEY_S)) game.player.position.y += 5;
+
+        game.player.body.x = game.player.position.x;
+        game.player.body.y = game.player.position.y;
+
         BeginDrawing();
 
         ClearBackground(BLACK);
 
-        DrawText("HELLO COLORIST!", 220, 200, 30, WHITE);
+        DrawRectangleRec(game.player.body, BLUE);
 
         EndDrawing();
     }
