49fa04cf27bd612ea7a27c45a186ab41ab99b273
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-13 21:20:17 +0600

    added necessary creation functions(without coordinates)

--- src/map.h
+++ src/map.h
@@ -56,19 +56,19 @@ void LoadMap(Map &map)
     map.blue[0].texture = LoadTexture("assets/");
 
     map.blue[1].body;
-    map.blue[1].texture = LoadTexture("assets/platforms/floatb22");
+    map.blue[1].texture = LoadTexture("assets/platforms/floatb22.png");
 
     map.yellow[0].body;
-    map.yellow[0].texture = LoadTexture("assets/platforms/yellow1");
+    map.yellow[0].texture = LoadTexture("assets/platforms/yellow1.png");
 
     map.yellow[1].body;
-    map.yellow[1].texture = LoadTexture("assets/platforms/yellow2");
+    map.yellow[1].texture = LoadTexture("assets/platforms/yellow2.png");
 
     map.green[0].body;
-    map.green[0].texture = LoadTexture("assets/platforms/groundbg1");
+    map.green[0].texture = LoadTexture("assets/platforms/groundbg1.png");
 
     map.green[1].body;
-    map.green[1].texture = LoadTexture("assets/platforms/groundg2");
+    map.green[1].texture = LoadTexture("assets/platforms/groundg2.png");
 
     map.blueCount = 2;
     map.greenCount = 2;
@@ -94,8 +94,45 @@ void DrawMap(Map &map)
 {
     DrawTexture(map.background, 0, 0, WHITE);
 
+    Rectangle source;
+    Rectangle destination;
+
     for(int i = 0; i < map.blueCount; i++)
     {
+        source = {0, 0, (float)map.blue[i].texture.width, (float)map.blue[i].texture.height};
 
+        destination = map.blue[i].body;
+
+        DrawTexturePro(map.blue[i].texture, source, destination, {0,0}, 0, WHITE);
+    }
+
+    for(int i = 0; i < map.yellowCount; i++)
+    {
+        if(!map.yellow[i].broken)
+        {
+            source = {0, 0, (float)map.yellow[i].texture.width, (float)map.yellow[i].texture.height};
+
+            destination = map.yellow[i].body;
+
+            DrawTexturePro(map.yellow[i].texture, source, destination, {0,0}, 0, WHITE);
+        }
+    }
+
+    for(int i = 0; i < map.greenCount; i++)
+    {
+        source = {0, 0, (float)map.green[i].texture.width, (float)map.green[i].texture.height};
+
+        destination = map.green[i].body;
+
+        DrawTexturePro(map.green[i].texture, source, destination, {0,0}, 0, WHITE);
+    }
+
+    for(int i = 0; i < map.redCount; i++)
+    {
+        source = {0, 0, (float)map.red[i].texture.width, (float)map.red[i].texture.height};
+
+        destination = map.red[i].body;
+
+        DrawTexturePro(map.red[i].texture, source, destination, {0,0}, 0, WHITE);
     }
 }
