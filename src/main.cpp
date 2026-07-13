ab53b25ac6927640a789b45a2fe9cc8f453aab30
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-13 16:49:02 +0600

    Further Reordered Files

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+const float desiredWidth = 64.0f;
+const float desiredHeight = 64.0f;
+
 int main()
 {
     InitWindow(736, 501, "demo map e kahini korbo");
@@ -16,8 +19,6 @@ int main()
 
     // sprite er png file size e onek boro pura map parle cover kore felabe
     // for that reason eita variable rakhsi map ar sprite er size er ratio thik korte
-    // float desiredWidth = 64.0f;
-    // float desiredHeight = 64.0f;
 
     SetTargetFPS(60);
 
--- src/map.h
+++ src/map.h
@@ -3,7 +3,36 @@
 
 using namespace std;
 
-int main()
+struct Platform
 {
-    
+    Rectangle body;
+    Texture2D texture;
+};
+
+struct Map
+{
+    Texture2D background;
+
+    Platform platforms[100];
+
+    int platform_count;
+};
+
+void LoadMap(Map &map)
+{
+    map.background = LoadTexture("assets/background/bg.png");
+
+    map.platform_count = 3;
+
+    map.platforms[0].body = {0, 430, 250, 70};
+}
+
+void DrawMap(Map &map)
+{
+    DrawTexture(map.background, 0, 0, WHITE);
+
+    for(int i = 0; i < map.platform_count; i++)
+    {
+
+    }
 }
