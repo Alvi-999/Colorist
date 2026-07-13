5babaec222a54360a55ca33f5f146d168e887f99
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-13 20:37:07 +0600

    Added texture unload function

--- src/map.h
+++ src/map.h
@@ -50,31 +50,44 @@ struct Map
 
 void LoadMap(Map &map)
 {
-    map.background;
+    map.background = LoadTexture("assets/background/bg.png");
 
     map.blue[0].body;
-    map.blue[0].texture;
+    map.blue[0].texture = LoadTexture("assets/");
 
     map.blue[1].body;
-    map.blue[1].texture;
+    map.blue[1].texture = LoadTexture("assets/platforms/floatb22");
 
     map.yellow[0].body;
-    map.yellow[0].texture;
+    map.yellow[0].texture = LoadTexture("assets/platforms/yellow1");
 
     map.yellow[1].body;
-    map.yellow[1].texture;
+    map.yellow[1].texture = LoadTexture("assets/platforms/yellow2");
 
     map.green[0].body;
-    map.green[0].texture;
+    map.green[0].texture = LoadTexture("assets/platforms/groundbg1");
 
     map.green[1].body;
-    map.green[1].texture;
+    map.green[1].texture = LoadTexture("assets/platforms/groundg2");
 
     map.blueCount = 2;
     map.greenCount = 2;
     map.yellowCount = 2;
     map.redCount = 2;
 
+}
+
+void UnloadMap(Map &map)
+{
+    UnloadTexture(map.background);
+
+    for(int i = 0; i < map.blueCount; i++) UnloadTexture(map.blue[i].texture);
+    
+    for(int i = 0; i < map.yellowCount; i++) UnloadTexture(map.yellow[i].texture);
+
+    for(int i = 0; i < map.greenCount; i++) UnloadTexture(map.green[i].texture);
+
+    for(int i = 0; i < map.redCount; i++) UnloadTexture(map.red[i].texture);
 }
 
 void DrawMap(Map &map)
