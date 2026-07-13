8d5dd7e7f9758027343fe010e310e47c0ba0644c
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-13 20:23:34 +0600

    Added platform behavior stucts

--- src/map.h
+++ src/map.h
@@ -3,7 +3,31 @@
 
 using namespace std;
 
-struct Platform
+struct Blue
+{
+    Rectangle body;
+    Texture2D texture;
+
+    //blue things makes player float
+    float buoyancy;
+};
+
+struct Yellow
+{
+    Rectangle body;
+    Texture2D texture;
+
+    // temporary platform
+    bool broken;
+};
+
+struct Red
+{
+    Rectangle body;
+    Texture2D texture;
+};
+
+struct Green
 {
     Rectangle body;
     Texture2D texture;
@@ -13,25 +37,51 @@ struct Map
 {
     Texture2D background;
 
-    Platform platforms[100];
+    Blue blue[100];
+    Yellow yellow[100];
+    Red red[100];
+    Green green[100];
 
-    int platform_count;
+    int blueCount;
+    int yellowCount;
+    int redCount;
+    int greenCount;
 };
 
 void LoadMap(Map &map)
 {
-    map.background = LoadTexture("assets/background/bg.png");
+    map.background;
 
-    map.platform_count = 3;
+    map.blue[0].body;
+    map.blue[0].texture;
 
-    map.platforms[0].body = {0, 430, 250, 70};
+    map.blue[1].body;
+    map.blue[1].texture;
+
+    map.yellow[0].body;
+    map.yellow[0].texture;
+
+    map.yellow[1].body;
+    map.yellow[1].texture;
+
+    map.green[0].body;
+    map.green[0].texture;
+
+    map.green[1].body;
+    map.green[1].texture;
+
+    map.blueCount = 2;
+    map.greenCount = 2;
+    map.yellowCount = 2;
+    map.redCount = 2;
+
 }
 
 void DrawMap(Map &map)
 {
     DrawTexture(map.background, 0, 0, WHITE);
 
-    for(int i = 0; i < map.platform_count; i++)
+    for(int i = 0; i < map.blueCount; i++)
     {
 
     }
