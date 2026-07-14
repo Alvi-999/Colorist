00eded775d28242cd31915d99a024cd00a4c527e
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-14 20:33:24 +0600

    Updated collision.h

--- src/collision.h
+++ src/collision.h
@@ -4,8 +4,64 @@
 #include "map.h"
 #include "game.h"
 
-// void GreenCollision(Game &game)
-// {
+void LandPlayer(Player &player, Rectangle platform)
+{
+    player.position.y = platform.y - player.body.height;
 
-// }
+    player.velocity.y = 0;
 
+    player.grounded = true;
+
+    player.doubleJumpAvailable = true;
+
+    player.body.y = player.position.y;
+}
+
+void GreenCollision(Player &player, Map &map)
+{
+    player.grounded = false;
+
+    for(int i = 0; i < map.greenCount; i++)
+    {
+        if(CheckCollisionRecs(player.body, map.green[i].body))
+        {
+            LandPlayer(player, map.green[i].body);
+            break;
+        }
+    }
+}
+
+void RedCollision(Player &player, Map &map)
+{
+    for(int i = 0; i < map.redCount; i++)
+    {
+        if(CheckCollisionRecs(player.body, map.red[i].body))
+        {
+            LandPlayer(player, map.red[i].body);
+            break;
+        }
+    }
+}
+
+void YellowCollision(Player &player, Map &map)
+{
+    for(int i = 0; i < map.redCount; i++)
+    {
+        if(CheckCollisionRecs(player.body, map.yellow[i].body))
+        {
+            LandPlayer(player, map.yellow[i].body);
+            break;
+        }
+    }
+}
+
+void BlueCollision(Player &player, Map &map)
+{
+    for(int i = 0; i < map.blueCount; i++)
+    {
+        if(CheckCollisionRecs(player.body, map.blue[i].body))
+        {
+            player.velocity.y -= map.blue[i].buoyancy;
+        }
+    }
+}
