749e1e339c9b0218e7443ee9e979833c3e2c9921
Jaber Al Siddique Alvi <jasalvi139@gmail.com>
2026-07-20 00:17:40 +0600

    Implemented core collision engine for green platforms

--- src/collision.h
+++ src/collision.h
@@ -1,80 +1,157 @@
-#pragma once 
+#pragma once
 
 #include <raylib.h>
+#include "player.h"
 #include "map.h"
-#include "game.h"
 
-// void LandPlayer(Player &player, Rectangle platform)
-// {
-//     player.position.y = platform.y - player.body.height;
+//collision directions
+const int COLLISION_NONE = 0;
+const int COLLISION_TOP = 1;
+const int COLLISION_BOTTOM = 2;
+const int COLLISION_LEFT = 3;
+const int COLLISION_RIGHT = 4;
 
-//     player.velocity.y = 0;
-
-//     player.grounded = true;
-
-//     player.doubleJumpAvailable = true;
-
-//     player.body.y = player.position.y;
-// }
-
-void GreenCollision(Player &player, Map &map)
+bool isCollidiing(Player &player, Rectangle &platform)
 {
-    player.grounded = false;
+    return CheckCollisionRecs(player.body, platform);
+}
 
-    for(int i = 0; i < map.greenCount; i++)
+int GetCollisionSide(Player &player, Rectangle &platform)
+{
+    float playerLeft = player.body.x;
+    float playerRight = player.body.x + player.body.width;
+    float playerTop = player.body.y;
+    float playerBottom = player.body.y + player.body.height;
+
+    float platformLeft = platform.x;
+    float platformRight = platform.x + platform.width;
+    float platformTop = platform.y;
+    float platformBottom = platform.y + platform.height;
+
+    float leftOverlap = playerRight - platformLeft;
+    float rightOverlap = platformRight - playerLeft;
+    float topOverlap = playerBottom - platformTop;
+    float bottomOverlap = platformBottom - playerTop;
+
+    float minHorizontal = min(leftOverlap, rightOverlap);
+    float minVertical = min(topOverlap, bottomOverlap);
+
+    if (minVertical < minHorizontal)
     {
-        if(CheckCollisionRecs(player.body, map.green[i].body) && player.velocity.y > 0)
-        {
-            player.position.y = map.green[i].body.y - player.body.height;
-            player.velocity.y = 0;
-            player.grounded = true;
-            player.doubleJumpAvailable = true;
-            player.body.y = player.position.y;
-        }
+        if (topOverlap < bottomOverlap) return COLLISION_TOP;
+        else return COLLISION_BOTTOM;
+    }
+
+    if (minHorizontal < minVertical)
+    {
+        if (leftOverlap < rightOverlap) return COLLISION_LEFT;
+        else return COLLISION_RIGHT;
+    }
+
+    if (fabs(player.velocity.y) >= fabs(player.velocity.x))
+    {
+        if (player.velocity.y > 0) return COLLISION_TOP;
+        else return COLLISION_BOTTOM;
+    }
+    else
+    {
+        if (player.velocity.x > 0) return COLLISION_LEFT;
+        else return COLLISION_RIGHT;
     }
 }
 
-void RedCollision(Player &player, Map &map)
+
+void ResolveCollision(Player &player, Rectangle &platform, int side)
+{
+    if(side == COLLISION_TOP)
+    {
+        player.body.y = platform.y - player.body.height;
+        player.position.y = player.body.y;
+
+        player.velocity.y = 0;
+
+        player.grounded = true;
+        player.doubleJumpAvailable = true;
+    }
+
+    else if(side == COLLISION_BOTTOM)
+    {
+        player.body.y = platform.y + platform.height;
+        player.position.y = player.body.y;
+
+        player.velocity.y = 0;
+    }
+
+    else if(side == COLLISION_LEFT)
+    {
+        player.body.x = platform.x - player.body.width;
+        player.position.x = player.body.x;
+
+        player.velocity.x = 0;
+    }
+
+    else if(side == COLLISION_RIGHT)
+    {
+        player.body.x = platform.x + platform.width;
+        player.position.x = player.body.x;
+
+        player.velocity.x = 0;
+    }
+}
+
+void GreenCollision(Player &player,Map &map)
+{
+    for(int i = 0; i < map.greenCount; i++)
+    {
+        Rectangle &platform = map.green[i].body;
+
+        if(isCollidiing(player, platform))
+        {
+            int side = GetCollisionSide(player, platform);
+
+            ResolveCollision(player, platform, side);
+        }
+    }
+}
+void RedCollision(Player &player,Map &map)
 {
     for(int i = 0; i < map.redCount; i++)
     {
-        if(CheckCollisionRecs(player.body, map.red[i].body) && player.velocity.y > 0)
+        Rectangle &platform = map.red[i].body;
+
+        if(isCollidiing(player, platform))
         {
-            player.position.y = map.red[i].body.y - player.body.height;
-            player.velocity.y = 0;
-            player.grounded = true;
-            player.doubleJumpAvailable = true;
-            player.body.y = player.position.y;
+            int side = GetCollisionSide(player, platform);
+
+            ResolveCollision(player, platform, side);
         }
     }
 }
-
 void YellowCollision(Player &player, Map &map)
 {
     for(int i = 0; i < map.yellowCount; i++)
     {
-        if(CheckCollisionRecs(player.body, map.yellow[i].body) && player.velocity.y > 0)
+        Rectangle &platform = map.yellow[i].body;
+
+        if(isCollidiing(player, platform))
         {
-            player.position.y = map.yellow[i].body.y - player.body.height;
-            player.velocity.y = 0;
-            player.grounded = true;
-            player.doubleJumpAvailable = true;
-            player.body.y = player.position.y;
+            int side = GetCollisionSide(player, platform);
+
+            ResolveCollision(player, platform, side);
         }
     }
 }
-
-void BlueCollision(Player &player, Map &map)
+void BlueCollision(Player &player,Map &map)
 {
     for(int i = 0; i < map.blueCount; i++)
     {
-        if(CheckCollisionRecs(player.body, map.blue[i].body) && player.velocity.y > 0)
+        Rectangle &platform = map.blue[i].body;
+
+        if(isCollidiing(player, platform))
         {
-            player.position.y = map.blue[i].body.y - player.body.height;
-            player.velocity.y = 0;
-            player.grounded = true;
-            player.doubleJumpAvailable = true;
-            player.body.y = player.position.y;
+            int side = GetCollisionSide(player, platform);
+
+            ResolveCollision(player, platform, side);
         }
     }
 }
--- src/map.h
+++ src/map.h
@@ -10,7 +10,6 @@ struct Blue
     Rectangle body;
     Texture2D texture;
 
-    //blue things makes player float
     float buoyancy;
 };
 
@@ -19,7 +18,6 @@ struct Yellow
     Rectangle body;
     Texture2D texture;
 
-    // temporary platform
     bool broken;
 };
 
@@ -52,31 +50,6 @@ struct Map
 
 void LoadMap(Map &map)
 {
-    // map.background = LoadTexture("assets/background/bg.png");
-
-    // map.blue[0].body;
-    // map.blue[0].texture = LoadTexture("assets/");
-
-    // map.blue[1].body;
-    // map.blue[1].texture = LoadTexture("assets/platforms/floatb22.png");
-
-    // map.yellow[0].body;
-    // map.yellow[0].texture = LoadTexture("assets/platforms/yellow1.png");
-
-    // map.yellow[1].body;
-    // map.yellow[1].texture = LoadTexture("assets/platforms/yellow2.png");
-
-    // map.green[0].body;
-    // map.green[0].texture = LoadTexture("assets/platforms/groundbg1.png");
-
-    // map.green[1].body;
-    // map.green[1].texture = LoadTexture("assets/platforms/groundg2.png");
-
-    // map.blueCount = 2;
-    // map.greenCount = 2;
-    // map.yellowCount = 2;
-    // map.redCount = 2;
-
     map.greenCount = 2;
     map.redCount = 1;
     map.blueCount = 1;
@@ -115,21 +88,8 @@ void UnloadMap(Map &map)
 
 void DrawMap(Map &map)
 {
-    // DrawTexture(map.background, 0, 0, WHITE);
-
-    
-
-    // Rectangle source;
-    // Rectangle destination;
-
     for(int i = 0; i < map.blueCount; i++)
     {
-        // source = {0, 0, (float)map.blue[i].texture.width, (float)map.blue[i].texture.height};
-
-        // destination = map.blue[i].body;
-
-        // DrawTexturePro(map.blue[i].texture, source, destination, {0,0}, 0, WHITE);
-
         DrawRectangleRec(map.blue[i].body, BLUE);
 
     }
@@ -138,34 +98,17 @@ void DrawMap(Map &map)
     {
         if(!map.yellow[i].broken)
         {
-            // source = {0, 0, (float)map.yellow[i].texture.width, (float)map.yellow[i].texture.height};
-
-            // destination = map.yellow[i].body;
-
-        //     DrawTexturePro(map.yellow[i].texture, source, destination, {0,0}, 0, WHITE);
-            
             DrawRectangleRec(map.yellow[i].body, YELLOW);
         }
     }
 
     for(int i = 0; i < map.greenCount; i++)
     {
-        // source = {0, 0, (float)map.green[i].texture.width, (float)map.green[i].texture.height};
-
-        // destination = map.green[i].body;
-
-        // DrawTexturePro(map.green[i].texture, source, destination, {0,0}, 0, WHITE);
-    
         DrawRectangleRec(map.green[i].body, GREEN);
     }
 
     for(int i = 0; i < map.redCount; i++)
-    {
-        // source = {0, 0, (float)map.red[i].texture.width, (float)map.red[i].texture.height};
-
-        // destination = map.red[i].body;
-
-        // DrawTexturePro(map.red[i].texture, source, destination, {0,0}, 0, WHITE);        
+    {        
         DrawRectangleRec(map.red[i].body, RED);
     }
 }
