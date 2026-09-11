70b67a27ccc1997d457e48d1df16fee9531fce4c
Niamul Mahbub <niamulmahbub@gmail.com>
2026-09-11 23:48:22 +0600

    Implemented basic camera

--- src/main.c
+++ src/main.c
@@ -3,28 +3,36 @@
 #include "player.h"
 #include "game.h"
 #include "map.h"
+#include "camera.h"
 
 int main()
 {
-    InitWindow(1280, 720, "Colorist");
+    InitWindow(1920, 1080, "Colorist");
     SetTargetFPS(60);
 
     Player player;
     Map map;
+    Camera2D MainCamera;
     
     InitializePlayer(&player);
 
     LoadMap(&map);
 
+    InitializeCamera(&MainCamera, &player);
+
     while(!WindowShouldClose())
     {
         UpdateGame(&player, &map);
 
+        UpdateMainCamera(&MainCamera, &player);
+
         BeginDrawing();
 
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
 
-        DrawGame(&player, &map);
+        BeginMode2D(MainCamera);
+            DrawGame(&player, &map);
+        EndMode2D();
 
         EndDrawing();
     }
