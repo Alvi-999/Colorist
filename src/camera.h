32a7ab0ff7c48f2f80f84060e835167c121f99ad
Niamul Mahbub <niamulmahbub@gmail.com>
2026-09-13 03:55:57 +0600

    Added boundary in camera

--- src/camera.h
+++ src/camera.h
@@ -17,7 +17,20 @@ void UpdateMainCamera(Camera2D *MainCamera, Player *player)
 {
     float screenWidth = (float)GetScreenWidth();
     float screenHeight = (float)GetScreenHeight();
-    MainCamera->target = (Vector2){player->position.x, player->position.y};
+
+    float CameraCenterX = max(player->position.x, 1920.0/2);
+    CameraCenterX = min(CameraCenterX, 5760.0-(1920.0/2));
+
+    float CameraCenterY = max(player->position.y, 1080.0/2);
+
+    if(CameraCenterY > 2160)
+    {
+        player->position.x = 546;
+        player->position.y = 76;
+        Menu_State = MENU_MAIN;
+    }
+
+    MainCamera->target = (Vector2){CameraCenterX, CameraCenterY};
     MainCamera->offset = (Vector2){(float)screenWidth/2, (float)screenHeight/2};
 }
 
